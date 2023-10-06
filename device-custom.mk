59883a84fe275ba607a4d242447f51ff9a7cf891
Michael Bestas <mkbestas@gmail.com>
2023-10-06 20:03:59 +0300

    caimito: Include wireless_charger makefile
    This includes the required sepolicy and compatibility matrix
    in the build.
    
    Change-Id: I3096357c5f45e664a7b39870fdc053e48d1361ad

--- device-custom.mk
+++ device-custom.mk
@@ -7,3 +7,6 @@
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# wireless_charger HAL service
+include device/google/gs-common/wireless_charger/wireless_charger.mk
