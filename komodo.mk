becad7e80a1500844a18c475a1d337dc9a6de3d7
oberdfr <alebennella@gmail.com>
2024-09-01 22:25:26 +0200

    komodo: initialize PixelApps vars before calling config makefile

--- komodo.mk
+++ komodo.mk
@@ -20,6 +20,12 @@ USE_SWIFTSHADER := false
 BOARD_USES_SWIFTSHADER := false
 RELEASE_PIXEL_AIDL_AUDIO_HAL := true
 
+# PixelApps
+TARGET_PREBUILT_PIXELAPPS := true
+PIXELAPPS_UDFPS := true
+PIXELAPPS_ULTRASONIC_UDFPS := true
+PIXELAPPS_OPTIONAL := true
+
 $(call inherit-product, device/google/zumapro/hentai_common.mk)
 $(call inherit-product, device/google/caimito/device-komodo.mk)
 $(call inherit-product, device/google/caimito/device-custom.mk)
@@ -38,9 +44,3 @@ $(call inherit-product,  device/google/caimito/komodo/props.mk)
 
 # Extracted vendor Blobs
 $(call inherit-product, vendor/google/komodo/komodo-vendor.mk)
-
-# PixelApps
-TARGET_PREBUILT_PIXELAPPS := true
-PIXELAPPS_UDFPS := true
-PIXELAPPS_ULTRASONIC_UDFPS := true
-PIXELAPPS_OPTIONAL := true
